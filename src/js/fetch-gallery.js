b8b92e48030e2366f4814226dc509c1a405e5e51
AnastasiiaSteblyna <zanegina93@gmail.com>
2022-10-26 14:25:53 +0300

    Наче працює

--- src/js/fetch-gallery.js
+++ src/js/fetch-gallery.js
@@ -21,27 +21,26 @@ export default class ApiService {
         );
         return;
       } else {
-        this.page += 1;
-
-        Notiflix.Notify.info(`Hooray! We found ${data.data.total} images.`);
+        if (this.page === 1) {
+          Notiflix.Notify.info(`Hooray! We found ${data.data.total} images.`);
+        }
 
         this.totalPage = Math.ceil(data.data.totalHits / 40);
 
-        if (
-          this.page < this.totalPage &&
-          refs.loadMoreBtn.classList.contains('is-hidden')
-        ) {
+        if (this.page < this.totalPage) {
           refs.loadMoreBtn.classList.remove('is-hidden');
-        } else if (!refs.loadMoreBtn.classList.contains('is-hidden')) {
+        } else {
           refs.loadMoreBtn.classList.add('is-hidden');
         }
 
-        if (this.totalPage > 1 && this.page === this.totalPage) {
+        if (this.page === this.totalPage && this.totalPage > 1) {
           Notiflix.Notify.warning(
             "We're sorry, but you've reached the end of search results."
           );
-          // refs.loadMoreBtn.classList.add('is-hidden');
+          refs.loadMoreBtn.classList.add('is-hidden');
         }
+
+        this.page += 1;
 
         return data.data.hits;
       }
